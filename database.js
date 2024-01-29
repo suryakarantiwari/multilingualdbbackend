735df98e5123d4a00f40ec96431040169bd15e47
suryakarantiwari <suryakaran.tiwari@blazeclan.com>
2024-01-29 17:11:22 +0530

    connect string

--- database.js
+++ database.js
@@ -1,12 +1,14 @@
 const { Client } = require('pg');
 require('dotenv').config();
 
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 const client = new Client({
-    host: process.env.DB_HOST,
-    port : process.env.DB_PORT,
-    user : process.env.DB_USER,
-    password : process.env.DB_PASSWORD,
-    database : process.env.DB_DATABASE
+    // host: process.env.DB_HOST,
+    // port : process.env.DB_PORT,
+    // user : process.env.DB_USER,
+    // password : process.env.DB_PASSWORD,
+    // database : process.env.DB_DATABASE
+    connectionString : connectionString,
 })
 
 module.exports = client;
